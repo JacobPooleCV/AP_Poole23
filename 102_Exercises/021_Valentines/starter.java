a046c256fa6c6223bdd205373ead83005fd76c47
JacobPooleCV <jpoole@gusd.net>
2023-02-14 11:43:55 -0800

    Valentines

---
+++ 102_Exercises/021_Valentines/starter.java
@@ -0,0 +1,18 @@
+import pkg.*;
+import java.util.*;
+import java.io.*;
+
+class starter {
+	public static void main(String args[]){
+        String yourPhrase = new String("REPLACE WITH YOUR PHRASE");
+        
+        Valentines love = new Valentines();
+        love.heart(yourPhrase);
+		
+        /*
+            while(true) {
+                i.loves(you);
+            }
+        */
+	}
+}
