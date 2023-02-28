c3a5670f6b0bd9b52a4a2e5e1834c8efff5473c8
JacobPoole <jacob.poole589@gmail.com>
2023-02-28 07:59:58 -0800

    change exercise

--- 102_Exercises/023_ArrayList_Add/starter.java
+++ 102_Exercises/023_ArrayList_Add/starter.java
@@ -1,4 +1,3 @@
-import pkg.*;
 import java.util.*;
 import java.io.*;
 
@@ -6,7 +5,11 @@ class starter {
 	public static void main(String args[]){
 		// Create an ArrayList
 		// Call addValuesArrayList on it with a user defined number of values (use Scanner)
-		// Call printArrayList it
+		// Call printArrayList on it
+
+		// Create a second ArrayList
+		// Call addValuesBackwards with the user input number of values
+		// Call printArrayList on it
 	}
 
 	public static void addValuesArrayList(ArrayList<Integer> arr, int n){
@@ -15,5 +18,9 @@ class starter {
 
 	public static void printArrayList(ArrayList<Integer> arr){
 		// This method should print all values in an ArrayList given.
+	}
+
+	public static void addValuesBackwards(ArrayList<Integer> arr, int n){
+		// This method should add values to the ArrayList in ascending order but print in descending order.
 	}
 }
