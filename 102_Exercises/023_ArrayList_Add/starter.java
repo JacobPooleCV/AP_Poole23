2aa047e5a1c8f1006c8faf824ea01e50c8481a3e
JacobPoole <jacob.poole589@gmail.com>
2023-02-28 07:35:18 -0800

    exercise arraylist add

---
+++ 102_Exercises/023_ArrayList_Add/starter.java
@@ -0,0 +1,19 @@
+import pkg.*;
+import java.util.*;
+import java.io.*;
+
+class starter {
+	public static void main(String args[]){
+		// Create an ArrayList
+		// Call addValuesArrayList on it with a user defined number of values (use Scanner)
+		// Call printArrayList it
+	}
+
+	public static void addValuesArrayList(ArrayList<Integer> arr, int n){
+		// This method should add n number of random values between 0-10 (10 not inclusive) to the ArrayList
+	}
+
+	public static void printArrayList(ArrayList<Integer> arr){
+		// This method should print all values in an ArrayList given.
+	}
+}
