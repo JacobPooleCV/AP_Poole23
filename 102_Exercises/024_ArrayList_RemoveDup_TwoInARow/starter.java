2d28bee5c59b74e0670f6e7dd458f8341083c3bd
JacobPooleCV <jpoole@gusd.net>
2023-03-02 10:44:27 -0800

    024 exercise

---
+++ 102_Exercises/024_ArrayList_RemoveDup_TwoInARow/starter.java
@@ -0,0 +1,33 @@
+import pkg.*;
+import java.util.*;
+
+class starter {
+	public static void main(String args[]) {
+		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+		addValuesInt(arr1, 100);
+		printValuesInt(arr1);
+		removeDup(arr1);		//Create this method.
+		System.out.println("There are " + twoInARow(arr1) + " values that are the same in a row.");
+		printValuesInt(arr1);
+	}
+
+	public static void removeDup(ArrayList<Integer> arr){
+		// This method should remove all duplicates within the ArrayList
+	}
+
+	public static int twoInARow(ArrayList<Integer> arr){
+		// This method should return the number of numbers that are the exact same in a row.
+	}
+
+	public static void addValuesInt(ArrayList<Integer> arr, int num){
+		for(int i = 0; i < num; i++){
+			arr.add(new Integer((int)(Math.random()*100)));
+		}
+	}
+
+	public static void printValuesInt(ArrayList<Integer> arr){
+		for(int i = 0; i < arr.size(); i++){
+			System.out.print(arr.get(i) + ", ");
+		}
+	}
+}
