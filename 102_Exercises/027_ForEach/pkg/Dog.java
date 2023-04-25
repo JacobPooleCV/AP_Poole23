8f0381ef746d79e1e57d0c1a6a2d050da87d6922
JacobPooleCV <jpoole@gusd.net>
2023-04-25 11:46:07 -0700

    Dog

---
+++ 102_Exercises/027_ForEach/pkg/Dog.java
@@ -0,0 +1,14 @@
+package pkg;
+
+public class Dog{
+	String name;
+	int age;
+
+	public Dog(){
+		name = "Potato";
+		age = 32;
+	}
+	public String toString(){
+		return "The dog name is " + name + " and its age is " + age;
+	}
+}
