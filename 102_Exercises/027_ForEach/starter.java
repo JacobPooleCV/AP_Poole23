f74561ced8638320d79979a8386138ce180e136c
JacobPooleCV <jpoole@gusd.net>
2023-04-25 12:50:23 -0700

    edits to 027

--- 102_Exercises/027_ForEach/starter.java
+++ 102_Exercises/027_ForEach/starter.java
@@ -1,4 +1,4 @@
-
+import pkg.*;
 public class starter{
 	public static void main(String[] args){
 		// Create a for each loop for the given Integers 2D Array
@@ -6,9 +6,19 @@ public class starter{
 
 		// Create a for each loop for the given Dog 2D Array
 		Dog[][] arr2 = new Dog[5][4];
+		populateDog(arr2);
 
 		// Create a for each loop to print each letter of the String 2D array on a new line
 		String[][] arr3 = {{"abc","def"},{"ghi", "jkl"},{"mno","pqr"}};
 
+	}
+
+	
+	public static void populateDog(Dog[][] arr){
+		for(int i = 0; i < arr.length; i++){
+			for(int j = 0; j < arr[0].length; j++){
+				arr[i][j] = new Dog();
+			}
+		}
 	}
 }
